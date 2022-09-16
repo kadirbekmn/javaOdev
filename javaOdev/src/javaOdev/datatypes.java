24706ad959398e55271bbf997cb977ca0b700c01
kadir <kadir@Kadir>
2022-09-16 23:42:12 +0300

    datatypes

---
+++ javaOdev/src/javaOdev/datatypes.java
@@ -0,0 +1,14 @@
+package javaOdev;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		double sayi = 12.5;
+		sayi = -129;
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+	}
+
+}
