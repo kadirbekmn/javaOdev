8674b1403026b65a8dc8963a3c1a7468bbc372ec
kadir <kadir@Kadir>
2022-09-16 23:39:21 +0300

    variables

---
+++ javaOdev/src/javaOdev/variables.java
@@ -0,0 +1,13 @@
+package javaOdev;
+
+public class variables {
+
+	public static void main(String[] args) {
+		String mesaj = "Öğrenci sayısı";
+		int ogrenciSayisi = 12;
+		
+		System.out.println(mesaj + " : " + ogrenciSayisi);
+				
+	}
+
+}
