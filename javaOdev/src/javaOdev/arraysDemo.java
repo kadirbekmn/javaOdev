0aa6a1be76cb37e7cd0ab80630610f8826afea9d
kadir <kadir@Kadir>
2022-09-17 15:47:15 +0300

    arraysDemo

---
+++ javaOdev/src/javaOdev/arraysDemo.java
@@ -0,0 +1,19 @@
+package javaOdev;
+
+public class arraysDemo {
+
+	public static void main(String[] args) {
+		
+		String[] ogrenciler = new String[4];
+		ogrenciler[0] = "Engin";
+		ogrenciler[1] = "Derin";
+		ogrenciler[2] = "Salih";
+		ogrenciler[3] = "Ahmet";
+		
+		for(int i = 0; i < ogrenciler.length; i ++) {
+			System.out.println(ogrenciler[i]);
+		}
+		
+	}
+
+}
