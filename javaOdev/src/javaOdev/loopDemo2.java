07065b98dacc54d2b9221243de770574602d0cef
kadir <kadir@Kadir>
2022-09-17 15:25:23 +0300

    loopDemo2

---
+++ javaOdev/src/javaOdev/loopDemo2.java
@@ -0,0 +1,16 @@
+package javaOdev;
+
+public class loopDemo2 {
+
+	public static void main(String[] args) {
+		
+		int i = 5;
+		
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While döngüsü bitti");
+	}
+
+}
