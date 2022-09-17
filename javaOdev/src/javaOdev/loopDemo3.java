c422b6b6782f8c54d466d952550e6c338486846e
kadir <kadir@Kadir>
2022-09-17 15:42:42 +0300

    loopDemo3

---
+++ javaOdev/src/javaOdev/loopDemo3.java
@@ -0,0 +1,16 @@
+package javaOdev;
+
+public class loopDemo3 {
+
+	public static void main(String[] args) {
+
+		int j = 10;
+
+		do {
+			System.out.println(j);
+			j += 2;
+		} while (j < 20);
+
+	}
+
+}
