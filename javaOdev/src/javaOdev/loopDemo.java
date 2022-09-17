bdb1e8c75e6495a3868349bdd7adbd40dfd88008
kadir <kadir@Kadir>
2022-09-17 15:23:14 +0300

    loopDemo

---
+++ javaOdev/src/javaOdev/loopDemo.java
@@ -0,0 +1,13 @@
+package javaOdev;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i < 10; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü bitti");
+	}
+
+}
