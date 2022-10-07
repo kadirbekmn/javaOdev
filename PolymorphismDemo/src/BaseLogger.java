6e49fbaaadcca9ae670fe9c47ace2050a28b749a
kadir <kadir@Kadir>
2022-10-08 00:38:28 +0300

    3. hafta ödev

---
+++ PolymorphismDemo/src/BaseLogger.java
@@ -0,0 +1,10 @@
+
+public class BaseLogger {
+	
+	public void log(String message) {
+		
+		System.out.println("Default logger : " + message);
+		
+	}
+
+}
---
+++ PolymorphismDemo/src/EmailLogger.java
@@ -0,0 +1,10 @@
+
+public class EmailLogger extends BaseLogger{
+
+	public void log(String message) {
+		
+		System.out.println("Logged to email : " + message);
+		
+	}
+	
+}
